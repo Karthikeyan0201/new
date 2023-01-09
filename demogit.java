10fa73012fec71d9310ec479bba71c0f016d8043
VIJAY199721 <122250780+VIJAY199721@users.noreply.github.com>
2023-01-09 11:59:52 +0530

    inteeger added

--- demogit.java
+++ demogit.java
@@ -1,5 +1,6 @@
 public class demogit {
     public static void main(String[] args){
+	    int a=10;
 	System.out.println("git initialized....");
 	 System.out.println("hai");
 	     System.out.println("hacker vijay");
