ebf491ff52daba3a36a61bb4bc1a606b01c1edbd
VIJAY199721 <122250780+VIJAY199721@users.noreply.github.com>
2023-01-09 11:49:23 +0530

    hacker vijay edited program

--- demogit.java
+++ demogit.java
@@ -2,6 +2,8 @@ public class demogit {
     public static void main(String[] args){
 	System.out.println("git initialized....");
 	 System.out.println("hai");
+	     System.out.println("hacker vijay");
+	    
 		}
 	
 	}
